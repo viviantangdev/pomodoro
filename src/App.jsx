a0b1f449c58c5df53e939b6648932012259d9c4d
Vivian Tang <viviantang.dev@gmail.com>
2025-08-15 17:09:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Pomodoro</h1>
+      <span>25</span>
+      <span>:</span>
+      <span>00</span>
+      <button type="button">start</button>
+      <button type="reset">reset</button>
+    </>
+  );
+}
+
+export default App;
