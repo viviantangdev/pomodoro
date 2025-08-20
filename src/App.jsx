561cdc701f314ac1b98215e05c14708f9e7352e7
Vivian Tang <viviantang.dev@gmail.com>
2025-08-20 16:46:06 +0200

    Changing work and breakminutes in settings

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,24 @@ import Timer from './components/Timer';
 import SettingsContext from './context/SettingsContext';
 
 function App() {
-  const [showSettings, setShowSettings] = useState(false);
+  const [showSettings, setShowSettings] = useState(true);
   const [workMinutes, setWorkMinutes] = useState(1);
   const [breakMinutes, setBreakMinutes] = useState(2);
   return (
     <>
       <h1>Pomodoro</h1>
-      <SettingsContext.Provider value={{
-        showSettings,
-        setShowSettings,
-        workMinutes,
-        breakMinutes,
-        setWorkMinutes,
-        setBreakMinutes,
-      }}>
-
-      {showSettings ? <Settings /> : <Timer />}
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        <Timer />
+        <Settings />
       </SettingsContext.Provider>
     </>
   );
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,15 +1,54 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import 'react-range-slider-input/dist/style.css';
 import SettingsContext from '../context/SettingsContext';
-
 const Settings = () => {
   const settingsInfo = useContext(SettingsContext);
 
+  const [newBreakMinues, setNewBreakMinutes] = useState(
+    settingsInfo.breakMinutes
+  );
+  const [newWorkMinutes, setNewWorkMinutes] = useState(
+    settingsInfo.workMinutes
+  );
+
+  function handleSave() {
+    settingsInfo.setWorkMinutes(newWorkMinutes);
+    settingsInfo.setBreakMinutes(newBreakMinues);
+  }
+
   return (
     <div className='flex flex-col space-x-2'>
-      <label>work minutes: {settingsInfo.workMinutes}</label>
+      <div>
+        <label>Work minutes: </label>
+        <input
+          type='number'
+          min={1}
+          value={newWorkMinutes}
+          onChange={(e) => setNewWorkMinutes(e.target.value)}
+        />
+      </div>
 
-      <label>break minutes: {settingsInfo.breakMinutes}</label>
-      <button type="button" onClick={() => settingsInfo.setShowSettings(false)}>Back</button>
+      <div>
+        <label>Break minutes: </label>
+        <input
+          type='number'
+          min={1}
+          value={newBreakMinues}
+          onChange={(e) => setNewBreakMinutes(e.target.value)}
+        />
+      </div>
+      <div>
+        {' '}
+        <button type='button' onClick={handleSave}>
+          Save
+        </button>
+        <button
+          type='button'
+          onClick={() => settingsInfo.setShowSettings(false)}
+        >
+          Back
+        </button>
+      </div>
     </div>
   );
 };
